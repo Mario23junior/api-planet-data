8b7f6c1ce7b0da0e42c4b0012bcaed2e2dce9edc
Mario23junior <mariojunior3251@gmail.com>
2021-03-04 06:35:29 -0300

    Listando informacoes Complementares do banco de dados

--- src/main/java/com/project/planet/Controller/ControllerComplementInformation.java
+++ src/main/java/com/project/planet/Controller/ControllerComplementInformation.java
@@ -1,11 +1,14 @@
 package com.project.planet.Controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.project.planet.Model.ComplementInformation;
 import com.project.planet.Repository.RepositoryComplementInformation;
@@ -25,4 +28,27 @@ public class ControllerComplementInformation {
 	public ComplementInformation salva(@RequestBody ComplementInformation complementInformation) {
 		return repositoryCompleInfor.save(complementInformation);
 	}
+	
+	@GetMapping("/{id}")
+	public ComplementInformation listById(@PathVariable Long id) {
+		return repositoryCompleInfor
+				    .findById(id)
+				    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NO_CONTENT,"informação não encontrado"));
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
