2d91d5ca5fc30f73ecead4c81c88ffa4cfadcd94
Mario23junior <mariojunior3251@gmail.com>
2021-03-04 05:06:27 -0300

    Repository de ComplementInformation de model ComplementInformation

---
+++ src/main/java/com/project/planet/Repository/RepositoryComplementInformation.java
@@ -0,0 +1,9 @@
+package com.project.planet.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.planet.Model.ComplementInformation;
+
+public interface RepositoryComplementInformation extends JpaRepository<ComplementInformation, Long> {
+
+}
