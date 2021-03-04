158e7cbabcd1dd22c8183125f758677c7bd0db07
Mario23junior <mariojunior3251@gmail.com>
2021-03-04 05:07:54 -0300

    Adicionando a notação Repository em ComplementInformation

--- src/main/java/com/project/planet/Repository/RepositoryComplementInformation.java
+++ src/main/java/com/project/planet/Repository/RepositoryComplementInformation.java
@@ -1,9 +1,11 @@
 package com.project.planet.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.project.planet.Model.ComplementInformation;
 
+@Repository
 public interface RepositoryComplementInformation extends JpaRepository<ComplementInformation, Long> {
 
 }
