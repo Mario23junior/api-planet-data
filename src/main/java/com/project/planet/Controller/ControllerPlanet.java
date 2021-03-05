9632a59d922fc76a64d4cd8eff3180b3309e6a6f
Mario23junior <mariojunior3251@gmail.com>
2021-03-05 05:15:00 -0300

    Atualizando dados do planeta

--- src/main/java/com/project/planet/Controller/ControllerPlanet.java
+++ src/main/java/com/project/planet/Controller/ControllerPlanet.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -47,6 +48,20 @@ public class ControllerPlanet {
 		            	  planetaRepository.delete(deletePlanet);
 		            	  return deletePlanet;
 		              }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NO_CONTENT,"Planeta não encontrado"));
+	}
+	
+	
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void update(@PathVariable Long id, @RequestBody Planeta planeta) {
+		 planetaRepository
+		               .findById(id)
+		               .map(updateId -> {
+		            	     updateId.setId(updateId.getId());
+		            	     planetaRepository.save(planeta);
+		            	     return updateId;
+		               }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NO_CONTENT));
+		 
 	}
 }
 
