8ea554dbc93f1afd8be86e3956c499ff017362e5
Mario23junior <mariojunior3251@gmail.com>
2021-03-05 01:50:18 -0300

    Atualizando dados por id

--- src/main/java/com/project/planet/Controller/ControllerComplementInformation.java
+++ src/main/java/com/project/planet/Controller/ControllerComplementInformation.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -46,7 +47,20 @@ public class ControllerComplementInformation {
 				        .map(deleteId -> {
 				        	repositoryCompleInfor.delete(deleteId);
 				        	return deleteId;
-				        });
+				        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NO_CONTENT,"não existe informações complementares para deletar"));
+	}
+	
+	
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void update(@PathVariable Long id, @RequestBody ComplementInformation complementInformation) {
+		  repositoryCompleInfor 
+		                .findById(id)
+		                .map(updateData -> {
+		                	updateData.setId(updateData.getId());
+		                	repositoryCompleInfor.save(complementInformation);
+		                	return updateData;
+		                }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NO_CONTENT,"não existe dados para atualizar"));
 	}
 }
 
